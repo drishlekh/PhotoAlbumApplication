5c88dee2ad8b8884d492a8271618dde36da864f0
Drishtant <drish.lekh26@gmail.com>
2024-03-28 23:44:00 +0530

    xml files added

--- app/src/main/java/com/techmania/photoalbumapplication/view/MainActivity.kt
+++ app/src/main/java/com/techmania/photoalbumapplication/view/MainActivity.kt
@@ -5,9 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.techmania.photoalbumapplication.R
+import com.techmania.photoalbumapplication.viewmodel.MyImagesViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myImagesViewModel: MyImagesViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +23,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        myImagesViewModel= ViewModelProvider(this)[MyImagesViewModel::class.java]
+        myImagesViewModel.getAllImages().observe(this, Observer { images->
+            //update UI
+        })
     }
 }
